92c93ee603276279556bfea30eef66a7bef12f6f
Emil Nielsen <342703@RTS-365.dk>
2019-08-16 12:22:52 +0200

    header styling

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,6 +4,10 @@ module.exports = (app) =>{
 
     app.get('/', async (req,res) =>{
         res.send('hello world')
-    })
+    });
+
+    app.get("/header", async (req, res, next) => {
+        res.render("partials/header");
+    });
 
 }
