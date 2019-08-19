ef4e5079d8951460e3ee844de521e7a1790a08d8
Nicklas Spendler <xdanishgamerz@gmail.com>
2019-08-19 13:13:41 +0200

    lavet server validering

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -86,9 +86,6 @@ module.exports = (app) =>{
         res.redirect('/contact')
     })
 
-
-
-
     function validateEmail(email) {
         let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
