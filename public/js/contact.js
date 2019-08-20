c09a338531bd3954f2a12b684a51a2399e9eb643
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-08-20 09:15:43 +0200

    Contact form bug fixes, header logo link added

--- public/js/contact.js
+++ public/js/contact.js
@@ -5,50 +5,59 @@ let phone = contactForm.querySelector("#contactPhone");
 let subject = contactForm.querySelector("#contactSubject");
 let message = contactForm.querySelector("#contactMessage");
 
+function validateEmail(email) {
+	let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	return regex.test(email)
+}
+
 contactForm.addEventListener("submit", (event) => {
 	event.preventDefault();
-	
-	if(name.value != "" && typeof name.value == NaN) {
-		name.value = "";
+	let error = false;
+	if(name.value != "" && name.value.length > 1) {
 	}
 	else {
-		alert(`The name field must be filled in!`);
-		name.value = "";
+		alert(`Enter a name that's 2 or more characters!`);
+		error = true;
+		return false;
 	}
-	if(mail.value != "") {
-		mail.value = "";
+	if(mail.value != "" && validateEmail(mail.value)) {
 	}
 	else {
-		alert(`The mail field must be filled in!`);
-		mail.value = "";
+		alert(`Please enter a valid email!`);
+		error = true;
+		return false;
 	}
 	if(phone.value != "") {
 		if(phone.value.length == 8) {
-			phone.value = "";
 		}
 		else {
-			alert(`The phone number must be 8 digits long`);
-			phone.value = "";
+			alert(`Please enter a phone number that's 8 digits!`);
+			error = true;
+			return false;
 		}
 	}
 	else {
 		alert(`The phone field must be filled in!`);
-		phone.value = "";
+		error = true;
+		return false;
 	}
-	if(subject.value != "" && typeof subject.value == NaN) {
-		subject.value = "";
+	if(subject.value != "") {
 	}
 	else {
 		alert(`The subject field must be filled in!`);
-		subject.value = "";
+		error = true;
+		return false;
 	}
-	if(message.value != "" && typeof message.value == NaN) {
-		message.value = "";
+	if(message.value != "") {
 	}
 	else {
 		alert(`The message field must be filled in!`);
-		message.value = "";
+		error = true;
+		return false;
 	}
 
-
+	if (error == false){
+		contactForm.submit();
+	}
 });
